6fd0da581e48386d33e4f4f68fa6ef9363317935
Jason Yi <jasonyi@unc.edu>
2022-09-08 17:12:40 -0400

    Edits to ex03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,23 +1,26 @@
-"""EX03 - Structured Wordle"""
+"""EX03 - Structured Wordle."""
 
 __author__ = "730604615"
 
 
-def contain_char(word: str, letter: str) -> bool:
+def contains_char(word: str, letter: str) -> bool:
     """Given a word and a single character, return True if character is found in word"""
+
     assert len(letter) == 1
     flag = False
     i = 0
     while not flag and i < len(word):
-            if letter == word[i]:
-                flag = True
-                return flag
-            else:
-                i += 1
+        if letter == word[i]:
+            flag = True
+            return flag
+        else:
+            i += 1
     return flag
+
 
 def emojified(input_guess: str, secret_word: str) -> str:
     """Given the user's guess and the secret word, return a str of emoji boxes reflecting its match"""
+
     assert len(input_guess) == len(secret_word)
     counter: int = 0
     alt_counter: int = 0
@@ -31,7 +34,7 @@ def emojified(input_guess: str, secret_word: str) -> str:
         if input_guess[counter] == secret_word[counter]:
             box_emoji += GREEN_BOX
         else:
-            yellow_guess = contain_char(secret_word, input_guess[counter])
+            yellow_guess = contains_char(secret_word, input_guess[counter])
             if yellow_guess:
                 box_emoji += YELLOW_BOX
             else:
@@ -41,14 +44,19 @@ def emojified(input_guess: str, secret_word: str) -> str:
         yellow_guess = False
     return box_emoji
 
+
 def input_guess(length: int) -> str:
+    """Returns the user input based on the desired length"""
+
     user_input: str = input(f"Enter a {length}-letter guess: ")
     while len(user_input) != length:
         user_input = input("This was not 6 letters ! Try again: ")
     return user_input
 
+
 def main() -> None:
     """The entrypoint of the program and main game loop."""
+
     codes: str = "comet"
     n: int = 0
     while n < 6:
@@ -61,6 +69,7 @@ def main() -> None:
         else:
             n += 1
     print("X/6 - Sorry, try again tomorrow!")
+
 
 if __name__ == "__main__":
     main()
