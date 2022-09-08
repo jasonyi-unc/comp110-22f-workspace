1eafbbecdb2ab488f8cc712f6d6b2995b41162eb
Jason Yi <jasonyi@unc.edu>
2022-09-08 02:05:48 -0400

    Exercises + One Shot Wordle Completed

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,42 @@
+"""EX02 - One Shot Worldle: Loops!"""
+
+__author__ = "730604615"
+
+
+secret_word: str = "python"
+input_guess: str = input("What is your 6-letter guess? ")
+counter: int = 0
+box_emoji: str = ""
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+yellow_guess: bool = False
+alt_counter = 0
+
+
+while len(input_guess) != 6:
+  input_guess = input("This was not 6 letters ! Try again: ")
+
+
+while counter < len(secret_word):
+  if input_guess[counter] == secret_word[counter]:
+    box_emoji += GREEN_BOX      
+  else:
+    while yellow_guess == False and alt_counter < len(secret_word):
+        if input_guess[counter] == secret_word[alt_counter]:
+            yellow_guess = True
+        else:
+            alt_counter += 1
+    if yellow_guess:
+        box_emoji += YELLOW_BOX
+    else:
+        box_emoji += WHITE_BOX
+  counter += 1
+  alt_counter = 0
+  yellow_guess = False
+print(box_emoji)
+  
+if input_guess == secret_word:
+  print("Woo! You got it!")
+else:
+  print("Not quite. Play again soon!")
---
+++ lessons/functions.py
@@ -0,0 +1,10 @@
+from random import randint
+
+final_score: int = round(92.51)
+print(f"\n{final_score}")
+
+x: int = -110
+print(f"\n{abs(x)}")
+
+rolled_dice: int = randint(1, 6)
+print(f"\n{rolled_dice}")
