806b0fe6900d421dc15f966b557539ee67683eb4
Jason Yi <jasonyi@unc.edu>
2022-09-29 16:54:34 -0400

    Some updates

---
+++ exercises/cyoa.py
@@ -0,0 +1,17 @@
+"""EX06 - Choose Your Own Adventure!"""
+
+def main() -> None:
+    points: int = 0
+    player: str = ""
+    greet()
+
+
+def greet() -> None:
+    print("Welcome to the Adventure!")
+    player = input("What is your name? ")
+
+def points(p: int) -> int:
+    print("back")
+
+if __name__ == "__main__":
+    main()
